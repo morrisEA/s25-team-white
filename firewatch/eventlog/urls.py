6ba425020f236c7a4243e3afb3deeba4b5494314
lkanj001 <lkanj001@odu.edu>
2025-02-24 15:18:49 -0500

    Implemented RFID Reader in eventlog app

--- firewatch/eventlog/urls.py
+++ firewatch/eventlog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
 
 app_name = 'eventlog'
 urlpatterns = [
-    path("", views.eventlog_view, name="eventlog")
+    path("", views.eventlog_view, name="eventlog"),
+    path("scan/", views.scan_rfid, name="scan_rfid"), # endpoint for simulating RFID scan
+    path("scan_log", views.get_scan_log, name="scan_log"), # endpoint for retrieving scan history
+    
 ]
--- firewatch/eventlog/views.py
+++ firewatch/eventlog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .rfid_reader import RFIDReader # Import RFIDReader module
+
+# Create an instance of the RFID reader
+rfid_reader = RFIDReader()
 
 # Create your views here.
 def eventlog_view(request):
-    return render(request, "eventlog/eventlog.html")  
+    return render(request, "eventlog/eventlog.html")
+
+def scan_rfid(request):
+    """Simulate an RFID scan event and return the scanned tag details."""
+    scan_event = rfid_reader.generate_fake_scan()
+    return JsonResponse(scan_event)
+
+def get_scan_log(request):
+    """Retrieve all past RFID scan events."""
+    return JsonResponse({"scan_log": rfid_reader.get_scan_log()}) 
 
