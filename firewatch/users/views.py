d52662e3df355fe1e0058dfbac3c344ac9bfb4d0
Evan Morris <eamorris91@gmail.com>
2025-02-24 11:50:49 -0500

    authentication added

--- firewatch/users/views.py
+++ firewatch/users/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login")) 
+    return render(request, "users/dashboard.html")
+
+
 def login_view(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
 
     return render(request, "users/login.html")
+
+def logout_view(request):
+    user = request.user.username
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": f"{user} has logged out."
+    })
+
 
 
 
