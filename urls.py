bb3809074f0f8a0003e31276d87cf90259c26043
koalaOnEE <153156644+koalaOnEE@users.noreply.github.com>
2025-03-04 17:55:19 -0500

    Delete

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "armory"
-
-urlpatterns = [
-    path('watch/<int:watch_id>/check_out/', views.check_out_watch, name='watch_check_out'),
-    path('watch/<int:watch_id>/check_in/', views.check_in_watch, name='watch_check_in'),
-]
