587b7cd5c362aed7031c3ada35b7be6b84cb6831
lkanj001 <lkanj001@odu.edu>
2025-03-23 15:30:18 -0400

    Added Django-based RFID scan simulation view, form, template and added a Simulated RFID option to the nav bar.

--- firewatch/eventlog/views.py
+++ firewatch/eventlog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from armory.models import Watch
 
 # HTML view for the event log page
@@ -8,29 +9,30 @@ def eventlog_view(request):
         'watches': watches
     })  
 
-
 # View for the RFID scan simulation form
 import random
 from .forms import RFIDScanForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+# Django form-based view to simulate RFID scan
 def simulate_rfid_scan(request):
     """
     View to simulate RFID scan via a Django form.
     Generates a random serial number and submits it using a Django form.
     """
+
     generated_serial = f"FW-{random.randint(10000, 99999)}"
-    
+
     if request.method == "POST":
         form = RFIDScanForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("eventlog:eventlog"))
     else:
-        form = RFIDScanForm(initial={'serial_number': gernerated_serial})
+        # Pre-fill the form with the generated serial number
+        form = RFIDScanForm(initial={'serial_number': generated_serial})
 
     return render(request, "eventlog/simulate_rfid_scan.html", {
         "form": form
         })
-
