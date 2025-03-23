a4594daec5e938018b96838d60f3122fa02e2613
lkanj001 <lkanj001@odu.edu>
2025-03-23 13:45:45 -0400

    Removed API route for RFIDScanListCreateView

--- firewatch/eventlog/urls.py
+++ firewatch/eventlog/urls.py
@@ -2,16 +2,9 @@ from django.urls import path
 
 from . import views
 
-from .views import RFIDScanListCreateView
-
-
 app_name = 'eventlog'
 
 urlpatterns = [
     # Existing event log view
     path("", views.eventlog_view, name="eventlog"),
-
-    # New API endpoint for storing and retrieving RFID scan data
-    path("api/rfid-scans/", RFIDScanListCreateView.as_view(), name="rfid-scan-list-create"),
-    
 ]
